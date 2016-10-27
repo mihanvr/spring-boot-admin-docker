0f1b5419def4a5b0c207cf8a63f1c56ab19cc3da
Miha <mihanvr@gmail.com>
2016-10-27 18:00:54 +0300

    1.4.3

---
+++ src/main/java/docker/SpringBootAdminApplication.java
@@ -0,0 +1,18 @@
+package docker;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Miha on 27.10.2016.
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
